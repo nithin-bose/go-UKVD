c224873bbe0c5c8cdaa22646570c89c91e1e7680
Nithin Bose <nithinbose@gmail.com>
2017-06-26 20:36:23 +0530

    updated tests

--- ukvd_test.go
+++ ukvd_test.go
@@ -1,7 +1,6 @@
 package ukvd
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
@@ -13,47 +12,44 @@ func init() {
 	vd.SetLog(true)
 }
 
-func TestVDICheckBasicInvalidRegistration(t *testing.T) {
+func TestVDICheckBasicItemNotFound(t *testing.T) {
 	_, err := vd.VDICheckBasic("KLM123")
-	if err != KeyInvalidError {
+	if err != ItemNotFoundError {
 		t.Error(err)
 	}
 }
 
-func TestVDICheckFinanceInvalidRegistration(t *testing.T) {
+func TestVDICheckFinanceItemNotFound(t *testing.T) {
 	_, err := vd.VDICheckFinance("KLM123")
-	if err != KeyInvalidError {
+	if err != ItemNotFoundError {
 		t.Error(err)
 	}
 }
 
-func TestVDICheckFullInvalidRegistration(t *testing.T) {
+func TestVDICheckFullItemNotFound(t *testing.T) {
 	_, err := vd.VDICheckFull("KLM123")
-	if err != KeyInvalidError {
+	if err != ItemNotFoundError {
 		t.Error(err)
 	}
 }
 
 func TestVDICheckBasic(t *testing.T) {
-	details, err := vd.VDICheckBasic("KM12AKK")
+	_, err := vd.VDICheckBasic("KM12AKK")
 	if err != nil {
 		t.Error(err)
 	}
-	fmt.Println("details", details)
 }
 
 func TestVDICheckFinance(t *testing.T) {
-	details, err := vd.VDICheckFinance("KM12AKK")
+	_, err := vd.VDICheckFinance("KM12AKK")
 	if err != nil {
 		t.Error(err)
 	}
-	fmt.Println("details", details)
 }
 
 func TestVDICheckFull(t *testing.T) {
-	details, err := vd.VDICheckFull("KM12AKK")
+	_, err := vd.VDICheckFull("KM12AKK")
 	if err != nil {
 		t.Error(err)
 	}
-	fmt.Println("details", details)
 }
